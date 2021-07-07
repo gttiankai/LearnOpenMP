0bad5ebadb5fbecc12dad44abf65caa88171f5c0
lucasktian <lucasktian@tencent.com>
2021-07-07 09:56:44 +0800

    [UPD]1. omp task

--- src/linked_task.cpp
+++ src/linked_task.cpp
@@ -1,17 +1,3 @@
-// Tencent is pleased to support the open source community by making TNN available.
-//
-// Copyright (C) 2020 THL A29 Limited, a Tencent company. All rights reserved.
-//
-// Licensed under the BSD 3-Clause License (the "License"); you may not use this file except
-// in compliance with the License. You may obtain a copy of the License at
-//
-// https://opensource.org/licenses/BSD-3-Clause
-//
-// Unless required by applicable law or agreed to in writing, software distributed
-// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-// CONDITIONS OF ANY KIND, either express or implied. See the License for the
-// specific language governing permissions and limitations under the License.
-
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -25,7 +11,7 @@
 
 struct node {
   int data;
-  int fib_data;
+  int fibdata;
   struct node* next;
 };
 
@@ -43,7 +29,7 @@ int fib(int n) {
 void processwork(struct node* p) {
   int n;
   n = p->data;
-  p->fib_data = fib(n);
+  p->fibdata = fib(n);
 }
 
 struct node* init_list(struct node* p) {
@@ -54,18 +40,17 @@ struct node* init_list(struct node* p) {
   head = (struct node*)malloc(sizeof(struct node));
   p = head;
   p->data = FS;
-  p->fib_data = 0;
+  p->fibdata = 0;
   for (i = 0; i < N; i++) {
     temp = (struct node*)malloc(sizeof(struct node));
     p->next = temp;
     p = temp;
     p->data = FS + i + 1;
-    p->fib_data = i + 1;
+    p->fibdata = i + 1;
   }
   p->next = NULL;
   return head;
 }
-
 /**
 Process linked list
 Each linked list node will be processed by function 'processwork()'
@@ -78,7 +63,6 @@ Each linked list node will compute 5 fibonacci numbers beginning with 38
 43 : 433494437
 Compute Time: 7.087344 seconds
  * */
-
 int main(int argc, char* argv[]) {
   double start, end;
   struct node* p = NULL;
@@ -87,36 +71,26 @@ int main(int argc, char* argv[]) {
 
   printf("Process linked list\n");
   printf("  Each linked list node will be processed by function 'processwork()'\n");
-  printf("  Each linked list node will compute %d fibonacci numbers beginning with %d\n", N, FS);
+  printf("  Each ll node will compute %d fibonacci numbers beginning with %d\n", N, FS);
 
   p = init_list(p);
   head = p;
 
   start = omp_get_wtime();
+#pragma omp parallel default(none) firstprivate(p)
+#pragma omp single
   {
-    // count the number
-    int count = N + 1;
-    struct node* node_ptr[count];
-    for (int i = 0; i < count; ++i) {
-      node_ptr[i] = p;
+    while (p != NULL) {
+#pragma omp task
+      processwork(p);
       p = p->next;
     }
-#pragma omp parallel default(none) shared(count, node_ptr)
-    {
-#pragma omp single
-      printf(" %d threads \n", omp_get_num_threads());
-#pragma omp for schedule(static, 1)
-      for (int i = 0; i < count; ++i) {
-        processwork(node_ptr[i]);
-        //printf("thread number %d get index %d \n", omp_get_thread_num(), i);
-      }
-    };
   }
 
   end = omp_get_wtime();
   p = head;
   while (p != NULL) {
-    printf("%d : %d\n", p->data, p->fib_data);
+    printf("%d : %d\n", p->data, p->fibdata);
     temp = p->next;
     free(p);
     p = temp;
